081261495891979bd0367a0515e4372c0c9b7c42
WellingtonSilva12 <wellingtonsilva180g@gmail.com>
2022-07-29 15:44:39 -0300

    starting a new crud with authentication

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react'
+import Home from './pages/Home'
+import Login from './pages/Login'
+
+import firebaseApp from './services/firebase-config'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+
+const auth = getAuth(firebaseApp)
+
+function App() {
+  const [user, setUser] = useState(null)
+
+  onAuthStateChanged(auth, userFirebase => {
+    if (userFirebase) {
+      // sessio init
+      setUser(userFirebase)
+    } else {
+      // sessio no init
+      setUser(null)
+    }
+  })
+
+  return <div className="App">{user ? <Home /> : <Login />}</div>
+}
+
+export default App
